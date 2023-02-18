dbcb90655751f616a3ab59eea8cc82fb0210b4bc
lucascmpus <lucascmpus@gmail.com>
2023-02-18 18:48:09 -0300

    feat: input search

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useMemo, useState} from 'react';
 import Modal from 'react-modal';
 
 // Components
@@ -7,31 +7,50 @@ import {Card} from './components/Card';
 // Styles
 import './global/style.scss';
 import {useFetch} from './hooks/useFetch';
-import axios from "axios";
 import {Form} from "./components/Form";
 
 Modal.setAppElement('#root');
 
 export const App = () => {
 
-  const [modalIsOpen, setIsOpen] = useState(false);
+  const [modalIsOpen, setIsOpen] = useState<boolean>(false);
+  const [searchTerm, setSearchTerm] = useState<string>('');
+  const [searchTagChecked, setSearchTagChecked] = useState(false);
 
   function openModal() {
     setIsOpen(true);
-  };
+  }
 
   function closeModal() {
     setIsOpen(false)
-  };
+  }
 
   const {loading, data, error} = useFetch({
     url: 'tools',
     method: 'get'
   });
 
-  function addTool(e: any) {
-    console.log(e.target.value)
-  }
+  const searchResults = useMemo(() => {
+    const lowerSearchTerm = searchTerm.toLowerCase();
+    if (searchTagChecked) {
+      if (searchTerm === '') {
+        return data
+      } else {
+        let a = -1;
+        // TO-DO: Typing this Array
+        let ArrIdTags:any = []
+        data.map(({ tags, id }) => { ArrIdTags.push({ id: id, tags: tags }) })
+        const tagIncludes = ArrIdTags.filter(({id, tags}: any) => {
+          if (tags.includes(lowerSearchTerm)) {
+            a = id
+          } 
+        })    
+        return data.filter(({id})=> id === a)
+      }
+    } else {
+      return data.filter(({title}) => title.toLowerCase().includes(lowerSearchTerm))
+    }
+  }, [searchTerm, data])
 
   return (
     <div className='app'>
@@ -42,18 +61,28 @@ export const App = () => {
 
       <div className="commands">
         <div className="search-commands">
-          <input type="text" placeholder='search'/>
-          <input type="checkbox" id='checkbox'/>
+          <input
+            type="text"
+            placeholder='search'
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <input
+            type="checkbox"
+            id='checkbox'
+            onChange={(e) => setSearchTagChecked(!searchTagChecked)}
+          />
           <p>search in tags only</p>
         </div>
         <button onClick={openModal}>+ Add</button>
       </div>
 
       {loading
-        ? <p> Loading... </p>
-        : data.map(({id, title, description, link, tags}) => (
+        ? <p>Loading...</p>
+        : searchResults?.map(({id, title, description, link, tags}) => (
           <Card key={id} id={id} description={description} link={link} tags={tags} title={title}/>
-        ))}
+        ))
+      }
 
       <Modal
         isOpen={modalIsOpen}

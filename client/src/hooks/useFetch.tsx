7666723c23002b29441a019f7359fe456a1d8de8
lucascmpus <lucascmpus@gmail.com>
2023-02-22 01:33:20 -0300

    refactor: refactor stats

--- client/src/hooks/useFetch.tsx
+++ client/src/hooks/useFetch.tsx
@@ -1,31 +1,30 @@
 import axios from 'axios';
-import {useEffect, useState} from 'react';
+import {useEffect, useMemo, useState} from 'react';
 import {IData} from '../types/types';
 
 axios.defaults.baseURL = 'http://localhost:3000/';
 
 interface useFetchProps {
   url: string
-  method: string
 }
 
-const useFetch = ({url, method, ...rest}: useFetchProps) => {
+const useFetch = ({url}: useFetchProps) => {
   const [loading, setLoading] = useState<boolean>(true);
   const [data, setData] = useState<IData[]>([]);
   const [error, setError] = useState<Error | null>(null);
 
-  function fecthData() {
+  function fetchData() {
     axios.get(`${url}`)
-      .then((res) => setData(res.data))
+      .then((res) => setData( res.data ))
       .catch((err) => setError(err))
       .finally(() => setLoading(false));
   }
 
   useEffect(() => {
-    fecthData();
+    fetchData();
   }, []);
 
-  return {loading, data, error};
+  return {loading, data, error, setData};
 
 }
 

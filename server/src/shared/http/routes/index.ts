4077494ca722082b24ae25197953a54f31dbcbbb
lucascmpus <lucascmpus@gmail.com>
2023-03-05 20:53:12 -0300

    feat: delete routes

--- server/src/shared/http/routes/index.ts
+++ server/src/shared/http/routes/index.ts
@@ -7,10 +7,10 @@ const tools = new ToolsController();
 // Get All Tools
 router.get('/tools', tools.getAllTools);
 
-// Get One Tool
-router.get('/tools/:id', tools.getTools);
-
 // Create Tool
 router.post('/tools/', tools.createNewTools);
+
+// Delete Tool
+router.delete('/tools/:id', tools.deleteTools);
 
 export { router };
